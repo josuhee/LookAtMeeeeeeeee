1c7ab9b87d3c7d168ade1d5cce9a6ffbd8849982
josuhee <suheeqwe@gmail.com>
2022-06-08 13:19:41 +0900

    CV-07 Rotate Video 270

--- src/videoConvert.py
+++ src/videoConvert.py
@@ -1,23 +1,27 @@
-from numpy import source
-
-
 def graySacle(label, source_image):
+	import videoIO as vio
 	import cv2
-	import imageIO as iio
 
 	gray_image = cv2.cvtColor(source_image, cv2.COLOR_RGB2GRAY)
-	iio.show_image(label, gray_image)
+	vio.show_video(label, gray_image)
 
 def flip90(label, source_image):
-	import imageIO as iio
+	import videoIO as vio
 	import cv2
 
 	im_rotated = cv2.rotate(source_image, cv2.ROTATE_90_CLOCKWISE)
-	iio.show_image(label, im_rotated)
+	vio.show_video(label, im_rotated)
 
 def flip180(label, source_image):
-	import imageIO as iio
+	import videoIO as vio
 	import cv2
 
 	im_rotated = cv2.rotate(source_image, cv2.ROTATE_180)
-	iio.show_image(label, im_rotated)
+	vio.show_video(label, im_rotated)
+
+def flip270(label, source_image):
+	import videoIO as vio
+	import cv2
+
+	im_rotated = cv2.rotate(source_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+	vio.show_video(label, im_rotated)
